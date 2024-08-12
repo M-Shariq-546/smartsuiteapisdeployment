2e9f2adf998c54377749537f5a4753ee9de2033a
Muhammad Shariq Shafiq <45226@gcslahore.edu.pk>
2024-08-12 12:28:21 +0500

    Commit Issue removed

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,9 +1,6 @@
 from rest_framework import serializers
 from accounts.models import CustomUser, CustomDepartmentStudent, CustomDepartmentTeacher
-<<<<<<< HEAD
-=======
 # from datetime import datetime
->>>>>>> 07a3d40485f80fe6d998cd5cb05da7e7b8da65c4
 import datetime
 from .utils import *
 from threading import Thread
