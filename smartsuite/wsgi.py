da69b3cb00813f401cada8e75812b57138bce650
Muhammad Shariq Shafiq <45226@gcslahore.edu.pk>
2024-08-21 13:16:44 +0500

    Update wsgi.py

--- smartsuite/wsgi.py
+++ smartsuite/wsgi.py
@@ -13,6 +13,12 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartsuite.settings')
 
+import os
+from dotenv import load_dotenv
+
+smartsuite_folder = os.path.join(os.getcwd(), 'smartsuite')  # or use os.path.abspath('smartsuite')
+load_dotenv(os.path.join(smartsuite_folder, '.env'))
+
 application = get_wsgi_application()
 
 app = application
