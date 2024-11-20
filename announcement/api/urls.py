a29f06adc6e181569e9757403176f6bf69a4e119
45226@gcslahore.edu.pk <45226@gcslahore.edu.pk>
2024-11-20 23:45:11 +0500

    Announcements types api created for frontend usage

--- announcement/api/urls.py
+++ announcement/api/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('announcement/', AnnouncementCreateAPIView.as_view(), name='announcement-list'),
+    path('announcement-types/', AnnouncementTypesView.as_view(), name='types'),
 ]
