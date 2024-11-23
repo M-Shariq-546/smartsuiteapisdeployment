a10c9bd3498715831874e19cd110df12d56a101d
M-Shariq-546 <45226@gcslahore.edu.pk>
2024-11-23 15:49:50 +0500

    name issue ficed

--- announcement/models.py
+++ announcement/models.py
@@ -21,7 +21,7 @@ class Accouncements(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return self.title 
+        return self.name 
     
     class Meta:
         verbose_name = 'Announcement'
